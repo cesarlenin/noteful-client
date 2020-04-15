96407bb252216b2417da7f7fa2204013b2ee2217
Cesar Morales <cesarlenin2@live.com>
2020-04-15 09:24:57 -0700

    added context for notes on pgs again

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -8,13 +8,11 @@ export default class MainPage extends Component {
     return (
       <main>
         <section className="folderList">
-        <FolderList/>
+          <FolderList/>
         </section>
         <section className="noteList ">
-        <NoteList 
-        notes={this.props.notes}
-        />
-        <button>Add notes</button>
+          <NoteList />
+          <button>Add notes</button>
         </section>
       </main>
     )
